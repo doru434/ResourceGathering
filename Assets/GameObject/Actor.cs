59e341b25f02313161cbee3ffb284e517fb83b27
doru434 <doru434@gmail.com>
2018-05-02 16:03:45 +0200

    repair

---
+++ Assets/GameObject/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+    public string name;
+    protected int hp;
+    public bool isSelected;
+    // Use this for initialization
+    protected virtual void Start () {
+		
+	}
+
+    // Update is called once per frame
+    protected virtual void Update () {
+		
+	}
+    public virtual void MoveObect(Vector3 Destination)
+    {
+
+    }
+}
---
+++ Assets/GameObject/Unit/Unit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : Actor {
+	// Use this for initialization
+	protected override void Start () {
+        base.Start();
+        isSelected = false;
+    }
+
+    // Update is called once per frame
+    protected override void Update () {
+        base.Update();     
+      
+      // Debug.Log(this.transform.name + this.transform.position);
+    }
+    public override void MoveObect(Vector3 destination)
+    {
+        base.MoveObect(destination);
+        Vector3 movement = new Vector3(0, 0, 0);
+        movement.x = destination.x;
+        movement.y = this.transform.position.y;
+        movement.z = destination.z;
+        
+     
+        this.transform.position = movement;
+    }
+}
